278712224d50ebee1fab212c5b43ae407f67edf0
Maxime Mouchet <max@maxmouchet.com>
2016-06-22 22:29:59 +0200

    Cleanup and handle meta/title markup

--- poc.py
+++ poc.py
@@ -5,7 +5,7 @@ import re
 import os
 os.chdir('koken-default-install/storage/themes/repertoire-fa8a5d39-01a5-dfd6-92ff-65a22af5d5ac/')
 
-data = {'featured_albums': [{'album': {'title': 'MON ALBUM'}}]}
+data = {'featured_albums': [{'album': {'title': 'MON ALBUM 1'}}, {'album': {'title': 'MON ALBUM 2'}}]}
 settings = {'settings': {'index_layout': 'one'}, 'site': {'title': 'MON SITE'}}
 
 class LensParser(HTMLParser):
@@ -24,6 +24,9 @@ class LensParser(HTMLParser):
         if self.record_loop:
             self.saved_loop += self.get_starttag_text()
 
+        elif tag == 'koken:asset':
+            self.output += self._koken_asset(dict(attrs))
+
         elif tag == 'koken:covers':
             # TODO: Load current album from stack
             covers = [{'index': 0, 'content': {'presets': {'medium_large': {'cropped': {'url': "TEST URL"}}}}}]
@@ -35,25 +38,30 @@ class LensParser(HTMLParser):
         elif tag == 'koken:load':
             data = self.stack[-1].get(dict(attrs)['source'])
             self.stack.append(data)
-            print(self.stack)
 
         elif tag == 'koken:loop':
             self.record_loop = True
 
         elif tag == 'koken:link':
-            print(self._koken_link(dict(attrs)))
             self.output += self._koken_link(dict(attrs))
 
-        elif tag == 'koken:shift':
+        elif tag == 'koken:shift'
             data = self.stack[-1][0]
             self.stack.append(data)
-            print(self.stack)
+
+        elif tag == 'koken:meta':
+            self.output += self._koken_meta(dict(attrs))
+
+        elif tag == 'koken:title':
+            self.output += self._koken_title(dict(attrs))
 
         else:
+            # TODO: Interpolate
             self.output += self.get_starttag_text()
 
     def handle_endtag(self, tag):
         if tag == 'koken:loop':
+            # TODO: Track loop nesting level to handle nested loops
             print('---- LOOP IN ----')
             for data in self.stack[-1]:
                 parser = LensParser(data, self.settings)
@@ -81,6 +89,14 @@ class LensParser(HTMLParser):
         else:
             self.output += self.interpolate_string(self.settings, data)
 
+    def _koken_asset(self, args):
+        # TODO: Handle js/img/... based on ext.
+        path = args['file']
+        if args.get('common'):
+            # TODO
+            path = '../../../app/site/themes/common/css/' + path
+        return "<link href=\"%(path)s\" type=\"text/css\" rel=\"stylesheet\" />" % locals()
+
     def _koken_include(self, args):
         path = self.interpolate_string(self.settings, args['file'])
 
@@ -89,13 +105,20 @@ class LensParser(HTMLParser):
 
         parser = LensParser(self.stack[0], self.settings)
         parser.feed(data)
-
         return parser.output
 
     def _koken_link(self, args):
         # TODO
         args['title'] = self.interpolate_string(self.stack[-1], args['title'])
         return "<a href=\"http://koken.me\">%(title)s</a>" % args
+
+    def _koken_meta(self, args):
+        # TODO: http://help.koken.me/customer/en/portal/articles/828690-lens-tags#kokenmeta
+        return "<meta charset=\"utf-8\">"
+
+    def _koken_title(self, args):
+        # TODO: http://help.koken.me/customer/en/portal/articles/828690-lens-tags#kokentitle
+        return "<title>TITLE TODO</title>"
 
     @staticmethod
     def value_for_variable(context, path):
